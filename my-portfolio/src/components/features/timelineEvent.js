24cc5727b600af0f90d697a9d0166ed2141bbc6f
Tioluwani <43251329+t1oluwani@users.noreply.github.com>
2024-08-28 00:12:38 -0600

    data -> timeline func

--- my-portfolio/src/components/features/timelineEvent.js
+++ my-portfolio/src/components/features/timelineEvent.js
@@ -1,28 +1,29 @@
 import '../styling/experience.css';
 
-// const TimelineEvent = (event) => {
-const TimelineEvent = () => {
+const TimelineEvent = ({event}) => {
 
-  const testEvent = 
-  {
-    "category": "education",
-    "start": "Sep 2017",
-    "end": "Jun 2020",
-    "organization": "Harry Ainlay Composite High School",
-    "title": "High School Diploma with Honours & IB Diploma",
-    "location": "Edmonton, Alberta, Canada",
-    "details": "Distinguised Service Award, Honors Award, IB Diploma Programme",
-    "skills": "Client Side Scripting, Leadership, CSS, Design, Japanese, English, HTML, JavaScript, Markup Languages, Critical Thinking"
-  }
+  console.log(event);
+
+  // const event =
+  // {
+  //   "category": "education",
+  //   "start": "Sep 2017",
+  //   "end": "Jun 2020",
+  //   "organization": "Harry Ainlay Composite High School",
+  //   "title": "High School Diploma with Honours & IB Diploma",
+  //   "location": "Edmonton, Alberta, Canada",
+  //   "details": "Distinguised Service Award, Honors Award, IB Diploma Programme",
+  //   "skills": "Client Side Scripting, Leadership, CSS, Design, Japanese, English, HTML, JavaScript, Markup Languages, Critical Thinking"
+  // }
 
   return (
-    <div className={`content ${testEvent.category}`}>
-      <p>{testEvent.start} - {testEvent.end}</p>
-      <p>{testEvent.location}</p>
-      <h2>{testEvent.organization}</h2>
-      <h3>{testEvent.title}</h3>
-      <p>{testEvent.details}</p>
-      <p>{testEvent.skills}</p>
+    <div className={`content ${event.category}`}>
+      <p className="times">{event.start} - {event.end}</p>
+      <p className="location">{event.location}</p>
+      <h2 className="organization">{event.organization}</h2>
+      <h3 className="title">{event.title}</h3>
+      <p className="details">{event.details}</p>
+      <p className="skills">{event.skills}</p>
     </div>
   );
 }
--- my-portfolio/src/components/pages/experience.js
+++ my-portfolio/src/components/pages/experience.js
@@ -1,5 +1,6 @@
 import '../styling/experience.css';
 import TimelineEvent from '../features/timelineEvent';
+import timelineData from '../../assets/experience/timelineData.json'
 
 const ExperiencePage = () => {
   return (
@@ -13,65 +14,9 @@ const ExperiencePage = () => {
       <div class="sectionBody">
         <div class="timeline">
           <div class="timeline-container">
-            <TimelineEvent />
-
-            {/* <div class="content education">
-              <p>Sep 2017 - Jun 2020</p>
-              <h2>Harry Ainlay High School</h2>
-              <p>High School Diploma</p>
-              <p>Grade: Honours</p>
-              <p>Activities: Varsity Soccer (2019), Varsity Track (2019-2020), Soccer Club (2017-2020)</p>
-              <p>Skills: Client Side Scripting, Leadership, CSS, Design, Japanese, English, HTML, JavaScript, Markup Languages, Critical Thinking</p>
-            </div>
-
-            <div class="content education">
-              <p>Sep 2018 - Jun 2020</p>
-              <h2>International Baccalaureate</h2>
-              <p>Grade: Diploma Awarded</p>
-              <p>Subjects: English Literature HL, Europe History HL, Physics HL, Japanese SL, Chemistry SL, Mathematics SL</p>
-            </div>
-
-            <div class="content education">
-              <p>Sep 2020 - Apr 2022</p>
-              <h2>The University of British Columbia</h2>
-              <p>Bachelor of Science - BS, CMS</p>
-              <p>Skills: R, JSON, OOP, Version Control, C, Racket, GitHub, Java, Data Structures, Git, C++</p>
-            </div>
-
-            <div class="content education">
-              <p>Sep 2022 - May 2025</p>
-              <h2>University of Alberta</h2>
-              <p>Bachelor of Science Honors - BS, Computing Science</p>
-              <p>Skills: Python, OOP, Version Control, C, GitHub, Data Structures, Git, Docker, Cybera, REST APIs</p>
-            </div>
-
-            <div class="content experience">
-              <p>Jul 2022 - Aug 2022 · 2 mos</p>
-              <h2>STEM Montessori Academy of Canada</h2>
-              <p>Virtual Summer Stem Facilitator</p>
-              <p>Toronto, Ontario, Canada · Remote</p>
-              <p>Virtual Coding Instructor for HTML/CSS</p>
-              <p>Skills: Markup Languages, HTML, Client Side Scripting, CSS, Teaching</p>
-            </div>
-
-            <div class="content experience">
-              <p>Nov 2022 - Jun 2023 · 8 mos</p>
-              <h2>STEM Montessori Academy of Canada</h2>
-              <p>Blanket Online Stem Facilitator</p>
-              <p>Toronto, Ontario, Canada · Remote</p>
-              <p>Virtual Coding Instructor for HTML/CSS, Python, and Math</p>
-              <p>Skills: Mathematics, Markup Languages, HTML, JavaScript, Python, Client Side Scripting, CSS, Teaching</p>
-            </div>
-
-            <div class="content experience">
-              <p>Jan 2024 - Apr 2024 · 4 mos</p>
-              <h2>Knead Technologies</h2>
-              <p>Student Software Developer</p>
-              <p>Calgary, Alberta, Canada · Remote</p>
-              <p>Full Stack Developer; fixed bugs, tested and developed new features for the recipient portal account</p>
-              <p>Skills: PHP, Laravel, TypeScript, React.js, MySQL, Docker, PHPUnit, Cypress, wsl, Sprint Planning, Git, GitHub</p>
-            </div> */}
-
+            {timelineData.map((event, idx) => (
+              <TimelineEvent event={event} key={idx} />
+            ))}
           </div>
         </div>
       </div>
