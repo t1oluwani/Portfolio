da2f11134c5325e0b2f7c601d03fb910cfd00580
Tioluwani <43251329+t1oluwani@users.noreply.github.com>
2025-09-14 16:13:29 -0600

    add starrynight back (rm by accident )

---
+++ src/components/features/starryNight.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import '../styling/starryNight.css';
+import Stars from './stars';
+
+const StarryNight = () => {
+
+  const [starNum, setStarNum] = useState(500);
+
+  const handleRangeChange = (event) => {
+    setStarNum(event.target.value);
+  };
+
+  useEffect(() => {
+    const starNum = document.getElementById('starNum');
+    const output = document.querySelector('output');
+
+    starNum.addEventListener('input', updateValue);
+
+    function updateValue(e) {
+      output.textContent = e.target.value;
+    }
+  }, [starNum]);
+
+
+  return (
+    <div className='starController'>
+      <div className='starSlider'>
+        <label htmlFor="starNum" className='starLabel'>
+          Number of Stars:&nbsp;
+          <output></output>
+        </label>
+        <input
+          type="range"
+          id="starNum"
+          min="1" max="1000"
+          value={starNum}
+          className='starNum'
+          onChange={handleRangeChange}
+        />
+      </div>
+      <Stars numberOfStars={starNum} />
+    </div>
+  );
+};
+
+export default StarryNight;
